4a8ac7063913c374c0312cf91a5ae96d11472d2b
sayheyhey <1834371139@qq.com>
2023-04-18 20:39:31 +0800

    TensorBoard使用

--- test_tb.py
+++ test_tb.py
@@ -3,8 +3,11 @@ import numpy as np
 from PIL import  Image
 
 writer = SummaryWriter("logs")
+image_path = 'train/ants/5650366_e22b7e1065.jpg'
+img_PIL = Image.open(image_path)
+img_array = np.array(img_PIL)
 
-writer.add_image()
+writer.add_image("train",img_array,2,dataformats='HWC')
 # y = x
 for i in range(100):
     writer.add_scalar("y=2x",2*i,i) # 标题，y轴，x轴
